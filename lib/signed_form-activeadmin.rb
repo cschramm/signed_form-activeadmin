4622d78c4665ac0d80e9e7fff5c00d8ab1e05933
Christopher Schramm <cschramm@shakaweb.org>
2014-01-13 12:08:17 +0100

    Wrap patches with initializer

--- lib/signed_form-activeadmin.rb
+++ lib/signed_form-activeadmin.rb
@@ -1,15 +1,23 @@
-module ActiveAdmin
-  class BaseController
-    include SignedForm::ActionController::PermitSignedParams
-  end
+module SignedForm
+  module ActiveAdmin
+    class Railtie < ::Rails::Railtie
+      initializer 'signed_form-activeadmin' do |_|
+        module ::ActiveAdmin
+          class BaseController
+            include SignedForm::ActionController::PermitSignedParams
+          end
 
-  module ViewHelpers
-    module FormHelper
-      alias_method :orig_active_admin_form_for, :active_admin_form_for
+          module ViewHelpers
+            module FormHelper
+              alias_method :orig_active_admin_form_for, :active_admin_form_for
 
-      define_method :active_admin_form_for do |resource, options = {}, &block|
-        options[:signed] = true
-        orig_active_admin_form_for resource, options, &block
+              define_method :active_admin_form_for do |resource, options = {}, &block|
+                options[:signed] = true
+                orig_active_admin_form_for resource, options, &block
+              end
+            end
+          end
+        end
       end
     end
   end
